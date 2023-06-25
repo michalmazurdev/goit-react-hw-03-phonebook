980859ed4e4d837a26bfc61b2483d862abc6967a
michalmazurdev <m.mazur40@gmail.com>
2023-06-25 14:01:16 +0200

    dependencies added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,80 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactItem } from './ContactItem/ContactItem';
+import { ContactList } from './ContactList/ContactList';
+import css from './App.module.css';
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    name: '',
+    filter: '',
+    number: '',
+  };
+
+  addContact = async event => {
+    event.preventDefault();
+    const { name, number, contacts } = this.state;
+    const contactNames = contacts.map(contact => contact.name);
+    if (contactNames.includes(name)) {
+      return alert(`${name} is alredy in contacts`);
+    }
+    await this.setState(prevState => ({
+      contacts: [
+        ...prevState.contacts,
+        { id: nanoid(), name: name, number: number.toString() },
+      ],
+    }));
+    event.target.reset();
+  };
+
+  handleChnage = event => {
+    console.log(event.target.name, event.target.value);
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  filterArrayByName = event => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  removeContact = id => {
+    const { contacts } = this.state;
+    this.setState({ contacts: contacts.filter(contact => contact.id !== id) });
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <h1 className={css.heading}>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} onChange={this.handleChnage} />
+        <h2 className={css.secondaryHeading}>Contacts</h2>
+        <Filter onChange={this.handleChnage} />
+        <ContactList>
+          <ContactItem
+            arrayOfContacts={this.filterArrayByName()}
+            deleteFunction={this.removeContact}
+          />
+        </ContactList>
+      </div>
+    );
+  }
+}
+export default App;
